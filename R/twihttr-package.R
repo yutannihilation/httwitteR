4ab7f747c638288685ad74634281f95d6614d3b2
Hiroaki Yutani <yutani.ini@gmail.com>
2016-11-30 12:08:36 +0900

    fix a bug

--- R/twihttr-package.R
+++ R/twihttr-package.R
@@ -4,5 +4,6 @@
 #'
 #' @docType package
 #' @seealso \url{https://dev.twitter.com/rest/public}, \url{https://github.com/hadley/httr/blob/master/demo/oauth1-twitter.r}
+#' @importFrom purrr %>%
 #' @name twihttr
 NULL
